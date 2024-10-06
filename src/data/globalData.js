54cecb7230697519519fdf24f34844cd181f6180
sarah <you@example.com>
2024-10-06 06:19:16 +0100

    initial commit

---
+++ src/data/globalData.js
@@ -0,0 +1,7 @@
+// Note: changing this requires restarting the server to reflect the changes
+module.exports = {
+  navigation: [
+    { title: "About", path: "/", fullMatch: true },
+    { title: "Posts", path: "/posts", fullMatch: false },
+  ],
+};
